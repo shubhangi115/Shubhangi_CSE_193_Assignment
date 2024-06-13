fcaaf6780d7a4bf7cd3636956f0e4651d4954be1
shubhangi <shubhangi1152005@gmail.com>
2024-06-13 23:02:04 +0530

    added

---
+++ Ques_5.py
@@ -0,0 +1,4 @@
+# Write a program that takes a string input from the user and writes it to a text file
+str=input("Enter string")
+print(str)
+sample_file=open(r'C:\Users\i5\OneDrive\Desktop\STRING INPUT FROM USER','w')
