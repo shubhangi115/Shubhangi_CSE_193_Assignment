29f7ee1d1ed677a3e6f74a5820269f574e288bf1
shubhangi <shubhangi1152005@gmail.com>
2024-06-14 15:14:09 +0530

    completed

--- Ques_14.py
+++
@@ -1,1 +0,0 @@
-# Write a program that reads multiple lines of input from the user until they enter an empty line, then prints all the lines.
--- Ques_5.py
+++ Ques_5.py
@@ -1,3 +1,4 @@
 # Write a program that takes a string input from the user and writes it to a text file
-# str=input("Enter string")
-# sample_file=open(r'C:\Users\i5\OneDrive\Desktop\STRING INPUT FROM USER\output_from user.txt','w')
+str1 = input("What do you like to write in your file? ")
+sample_file = open(r'C:\Users\i5\OneDrive\Desktop\STRING INPUT FROM USER\output_from user.txt', 'w')
+print(str1, file = sample_file)
